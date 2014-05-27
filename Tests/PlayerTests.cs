9bf550c6535fd984d7d389215f22dfcc289ef1ee
me-stefan-baiu <me.stefan.baiu@gmail.com>
2014-05-28 01:35:26 +0300

    added test example

---
+++ Tests/PlayerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SSSG.Models;
+
+namespace SSSGTests
+{
+    [TestClass]
+    public class PlayerTests
+    {
+        private Player player;
+        
+        [TestInitialize()]
+        public void setup()
+        {
+            player = new Player();
+        }
+
+        [TestCleanup()]
+        public void tearDown()
+        {
+        }
+        
+        [TestMethod]
+        public void DamageIsSubtracted()
+        {
+            int beforeLife = player.Health;
+            int damageAmount = 50;
+            player.TakeDamage(damageAmount);
+            int afterLife = player.Health;
+            Assert.AreEqual(beforeLife - damageAmount, afterLife);
+        }
+
+        [TestMethod]
+        public void PlayerCanDie()
+        {
+            Assert.IsTrue(player.IsAlive);
+            int damageAmount = player.Health;
+            player.TakeDamage(damageAmount);
+            Assert.IsFalse(player.IsAlive);
+        }
+
+    }
+}
